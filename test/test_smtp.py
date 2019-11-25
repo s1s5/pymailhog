d49364797b7166fcefc7806f24419c005ee44b4c
Symfo <symfoware@outlook.com>
2019-11-25 22:36:42 +0900

    add test

--- test/test_smtp.py
+++ test/test_smtp.py
@@ -1,23 +1,125 @@
-import base64
 import smtplib
-import ssl
-from email.mime.text import MIMEText
-from email.utils import formatdate
+import email.header
+import email.encoders
+import email.utils
+import email.mime
+import email.mime.text
+import email.mime.multipart
 
-import time
+import urllib.request
+import json
+
+import unittest
+
+class TestPyMailHogBasic(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self._delete_message(self)
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+        #self._delete_message(self)
+
+    def test_send_utf8(self):
+
+        main_text = 'UTF-8本文の確認テスト🍣🍺'
+        msg = email.mime.text.MIMEText(main_text, 'plain', 'utf-8')
+
+        msg['Subject'] = 'UTF-8件名のテスト🍣🍺'
+        msg['From'] = 'from@example.com'
+        msg['To'] = 'to@example.com'
+        msg['Cc'] = 'cc@example.com'
+        msg['Date'] = email.utils.formatdate(None,True)
+        msg['Message-ID'] = 'test_send_utf8'
+        self._send(msg)
+
+        rmsg = self._get_message(msg['Message-ID'])
+        self.assertEqual(msg['Subject'], rmsg['subject'])
+        self.assertEqual(msg['From'], rmsg['from'])
+        self.assertEqual([msg['To']], rmsg['to'])
+        self.assertEqual([msg['Cc']], rmsg['cc'])
+        self.assertEqual(main_text, rmsg['body'])
 
 
-main_text = "本文の確認テスト"
-msg = MIMEText(main_text, "plain", 'utf-8')
+    def test_send_sjis(self):
 
-msg["Subject"] = "件名のテスト"
-msg["From"] = "from@example.com"
-msg["To"] = "to@example.com"
-msg["Cc"] = ""
-msg["Bcc"] = ""
-msg["Date"] = formatdate(None,True)
-msg["Message-ID"] = str(time.time())
+        main_text = 'iso-2022-jp本文の確認テスト'
+        msg = email.mime.text.MIMEText(main_text, 'plain', 'iso-2022-jp')
 
-smtpclient = smtplib.SMTP('localhost', 1025)
-smtpclient.send_message(msg)
-smtpclient.quit()
+        subject = 'SJIS件名のテスト'
+        msg['Subject'] = email.header.Header(subject, 'iso-2022-jp')
+        msg['From'] = 'from@example.com'
+        msg['To'] = 'to@example.com'
+        msg['Cc'] = 'cc@example.com'
+        msg['Date'] = 'Mon, 25 Nov 2019 22:08:09 +0900'
+        msg['Message-ID'] = 'test_send_sjis'
+        self._send(msg)
+
+        rmsg = self._get_message(msg['Message-ID'])
+        self.assertEqual(msg['Subject'], rmsg['subject'])
+        self.assertEqual(msg['From'], rmsg['from'])
+        self.assertEqual([msg['To']], rmsg['to'])
+        self.assertEqual([msg['Cc']], rmsg['cc'])
+        self.assertEqual(main_text, rmsg['body'])
+        self.assertEqual('2019/11/25 22:08:09', rmsg['date'])
+
+
+    def test_sent_attachment(self):
+        msg = email.mime.multipart.MIMEMultipart()
+
+        msg['Subject'] = '添付ファイルあり'
+        msg['From'] = 'from@example.com'
+        msg['To'] = 'to@example.com'
+        msg['Cc'] = 'cc@example.com'
+        msg['Date'] = 'Mon, 25 Nov 2019 07:08:09 +0900'
+        msg['Message-ID'] = 'test_sent_attachment'
+
+        main_text = '添付ファイルの送信テスト(UTF8)'
+        msg.attach(email.mime.text.MIMEText(main_text, 'plain', 'utf-8'))
+
+
+        attachment = email.mime.base.MIMEBase('application', 'octet-stream')
+        attachment.set_payload('添付ファイルテスト'.encode('utf-8'))
+        email.encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', 'attachment; filename="attachment.txt"')
+        msg.attach(attachment)
+
+        self._send(msg)
+
+        rmsg = self._get_message(msg['Message-ID'])
+        self.assertEqual(msg['Subject'], rmsg['subject'])
+        self.assertEqual(msg['From'], rmsg['from'])
+        self.assertEqual([msg['To']], rmsg['to'])
+        self.assertEqual([msg['Cc']], rmsg['cc'])
+        self.assertEqual(main_text, rmsg['body'])
+        self.assertEqual('2019/11/25 07:08:09', rmsg['date'])
+
+        attachments = rmsg['attachments'][0]
+        self.assertEqual('attachment.txt', attachments['filename'])
+        
+
+
+    def _get_message(self, msg_id=None):
+        url = 'http://localhost:8025/api/v1/messages'
+        if msg_id:
+            url += '/'+msg_id
+        response = urllib.request.urlopen(url)
+        body = response.read().decode('utf-8')
+        return json.loads(body)
+
+    def _delete_message(self, msg_id=None):
+        url = 'http://localhost:8025/api/v1/messages'
+        if msg_id:
+            url += '/' + msg_id
+        req = urllib.request.Request(url, method='DELETE')
+        urllib.request.urlopen(req)
+
+    def _send(self, msg):
+        smtpclient = smtplib.SMTP('localhost', 1025)
+        smtpclient.send_message(msg)
+        smtpclient.quit()
+
+if __name__ == '__main__':
+    unittest.main()
